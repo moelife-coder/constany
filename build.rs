c049791e05e780ec6b0a8dbf7aedee825dd88e74
moelife-coder <61054382+moelife-coder@users.noreply.github.com>
2020-08-26 22:22:46 +0800

    A much longer readme

---
+++ build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    if let Ok(i) = std::env::var("NO_BUILD") {
+        if &i == "true" {
+            return;
+        }
+    }
+    std::process::Command::new(std::env::var("CARGO").unwrap())
+        .args(&["run", "--release", "--features", "stage_one"])
+        .env("NO_BUILD", "true")
+        .status()
+        .unwrap();
+    println!("cargo:rustc-cfg=stage_two");
+}
